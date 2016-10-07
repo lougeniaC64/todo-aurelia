86a77c21b4e9d672630f3601244626ab059b3643
Lougenia <lougenia.bailey@gmail.com>
2016-10-06 23:38:14 -0400

    Refactored constructors to allow for additional todo properties

--- src/todo.ts
+++ src/todo.ts
@@ -1,7 +1,22 @@
-export class Todo {
-  done = false;
-  dueDate = null;
-  category = null;
 
-  constructor(public description: string) { }
+interface ITodo {
+  done: boolean,
+  description: string
+}
+export class Todo implements ITodo {
+  done = false;
+  description = null;
+
+  constructor(todo: ITodo);
+  constructor(description: string, done: boolean);
+
+  constructor(todoOrDescription: string | ITodo, done?: boolean) {
+        if (typeof todoOrDescription === 'object') {
+            this.description = todoOrDescription.description;
+            this.done = done || false;
+        } else if (typeof todoOrDescription === 'string' && typeof done === 'boolean') {
+            this.description = todoOrDescription;
+            this.done = done;
+        }
+    }
 }
