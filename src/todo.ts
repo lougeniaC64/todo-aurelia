906a86ff4edb8dff089a2c59f8c449dc079fdb5c
Lougenia <lougenia.bailey@gmail.com>
2016-10-07 23:37:05 -0400

    Added done and dueDate properties to todo constructor

--- src/todo.ts
+++ src/todo.ts
@@ -6,17 +6,21 @@ interface ITodo {
 export class Todo implements ITodo {
   done = false;
   description = null;
+  dueDate = null;
 
   constructor(todo: ITodo);
-  constructor(description: string, done: boolean);
+  //constructor(description: string, done: boolean);
+  constructor(description: string, done: boolean, dueDate: Date);
 
-  constructor(todoOrDescription: string | ITodo, done?: boolean) {
+  constructor(todoOrDescription: string | ITodo, done?: boolean, dueDate?: Date) {
         if (typeof todoOrDescription === 'object') {
             this.description = todoOrDescription.description;
             this.done = done || false;
+            this.dueDate = dueDate || new Date();
         } else if (typeof todoOrDescription === 'string' && typeof done === 'boolean') {
             this.description = todoOrDescription;
             this.done = done;
+            this.dueDate = dueDate;
         }
     }
 }
