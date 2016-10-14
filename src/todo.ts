a3caa4267b18794888ac8d4ed7776bd819e80d0d
Lougenia <lougenia.bailey@gmail.com>
2016-10-13 21:15:32 -0400

    Removed commented code

--- src/todo.ts
+++ src/todo.ts
@@ -4,24 +4,17 @@ interface ITodo {
   done?: boolean,
   dueDate?: Date
 }
+
 export class Todo implements ITodo {
-  done = false;
-  description = null;
-  dueDate = null;
+    done = false;
+    description = null;
+    dueDate = null;
 
-  constructor(todo: ITodo);
-//   constructor(description: string, done: boolean);
-//   constructor(description: string, done: boolean, dueDate: Date);
+    constructor(todo: ITodo);
 
-  constructor(todo: ITodo) {
-        // if (typeof todoOrDescription === 'object') {
-            // this.description = todoOrDescription.description;
-            // this.done = done || false;
-            // this.dueDate = dueDate || new Date();
-        // } else if (typeof todoOrDescription === 'string' && typeof done === 'boolean') {
-            this.description = todo.description;
-            this.done = todo.done;
-            this.dueDate = todo.dueDate;
-        // }
+    constructor(todo: ITodo) {
+        this.description = todo.description;
+        this.done = todo.done;
+        this.dueDate = todo.dueDate;
     }
 }
